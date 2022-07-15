150170238144c40dacd65aca6428ec27f73b3f80
Noah Reece <65669992+ArkfulDodger@users.noreply.github.com>
2022-07-15 17:33:55 -0400

    Cube Rotates on Move
    Dice now rendered as object with six individual Quad meshes positioned around a point. This is rotated around a pivot point when the cube is moved. AFTER the movement, the parent position is updated and the mesh adjusted back to its place.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,12 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private PlayerControls _playerInput;
-    private Vector2 _moveInput;
-    private Vector3 _moveDirection;
+    private Vector3 _defaultChildPosition = new Vector3(0f, 0.5f, 0f);
+    [SerializeField] private Vector2 _moveInput;
+    [SerializeField] private Vector3 _moveDirection;
+    [SerializeField] private Transform _pivotPoint;
+    [SerializeField] private GameObject _dice;
+    [SerializeField] private Vector3 _pivotAxis;
 
     private void Awake()
     {
@@ -61,12 +65,32 @@ public class PlayerController : MonoBehaviour
     private void AttemptMove(Vector3 move)
     {
         if (IsMoveAllowed(move))
-            transform.position += move;
+            Move(move);
     }
 
     private bool IsMoveAllowed(Vector3 move)
     {
         return true;
+    }
+
+    private void Move(Vector3 move)
+    {
+        _pivotPoint.localPosition = move * 0.5f;
+        _pivotPoint.localRotation = Quaternion.LookRotation(move, Vector3.up);
+        _pivotAxis = move.z != 0 ? Vector3.right : Vector3.forward;
+        float multiplier = (_moveInput.x > 0 || _moveInput.y < 0) ? -1f : 1f;
+        //transform.RotateAround(Vector3.zero, move, 90f);
+        Debug.Log("pivot world: " + _pivotPoint.rotation.eulerAngles);
+        _dice.transform.RotateAround(_pivotPoint.position, _pivotAxis, 90f * multiplier);
+
+        MovePlayerAndResetDicePosition();
+    }
+
+    private void MovePlayerAndResetDicePosition()
+    {
+        transform.position += _moveDirection.normalized;
+        _pivotPoint.localPosition = _defaultChildPosition;
+        _dice.transform.localPosition = _defaultChildPosition;
     }
 
     
