0102d8f792bb0abcb910226fe2d06d2df92dcb58
Noah Reece <65669992+ArkfulDodger@users.noreply.github.com>
2022-07-16 14:56:53 -0400

    Added Goal Indicator and Level 01

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -8,6 +8,14 @@ public class Tile : MonoBehaviour
     public TileType Type { get { return _type; } }
     [SerializeField] private bool _isStart;
     [SerializeField] private bool _isEnd;
+    [SerializeField] private GameObject _goalIndicator;
     public bool IsEnd { get { return _isEnd; } }
 
+    private void Awake()
+    {
+        if (_isEnd)
+            _goalIndicator.SetActive(true);
+        else
+            _goalIndicator.SetActive(false);
+    }
 }
